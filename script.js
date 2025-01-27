8e86a32d05afa339e2e2cd72b5c680a22e5161e8
Paretoo <110358152+Paretoo@users.noreply.github.com>
2025-01-27 20:42:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// Defalut value
+window.onload = function () {
+  document.getElementById("counter1").value = score;
+  document.getElementById("counter2").value = score2;
+};
+
+// Variables
+var score = 0;
+var score2 = 0;
+
+// Counter 1 functions
+function redButton() {
+  document.getElementById("counter1").value = score += 1;
+}
+
+function yellowButton() {
+  document.getElementById("counter1").value = score += 2;
+}
+
+function greenButton() {
+  document.getElementById("counter1").value = score += 3;
+}
+
+function brownButton() {
+  document.getElementById("counter1").value = score += 4;
+}
+
+function blueButton() {
+  document.getElementById("counter1").value = score += 5;
+}
+
+function pinkButton() {
+  document.getElementById("counter1").value = score += 6;
+}
+
+function blackButton() {
+  document.getElementById("counter1").value = score += 7;
+}
+
+// Counter 2 functions
+function redButton2() {
+  document.getElementById("counter2").value = score2 += 1;
+}
+
+function yellowButton2() {
+  document.getElementById("counter2").value = score2 += 2;
+}
+
+function greenButton2() {
+  document.getElementById("counter2").value = score2 += 3;
+}
+
+function brownButton2() {
+  document.getElementById("counter2").value = score2 += 4;
+}
+
+function blueButton2() {
+  document.getElementById("counter2").value = score2 += 5;
+}
+
+function pinkButton2() {
+  document.getElementById("counter2").value = score2 += 6;
+}
+
+function blackButton2() {
+  document.getElementById("counter2").value = score2 += 7;
+}
+
+document.getElementById("reset").addEventListener("click", resetScore);
+function resetScore() {
+  document.getElementById("counter1").value = score = 0;
+  document.getElementById("counter2").value = score = 0;
+  alert("The game has been restarted!");
+}
